aeee7f38d065d3ca3713ff7a0ffda944f12d9843
Jaweria Niaz <61407664+jaweria332@users.noreply.github.com>
2020-10-10 17:34:26 +0500

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,39 +1,47 @@
 import cv2
-#import numpy as np
 import faceRecognition as fr
 
 
 #This module takes images  stored in diskand performs face recognition
-test_img=cv2.imread("E:\\TRY ON VIRTUAL SOFTWARE\\Step 1 Face Recognition\\test\\13.jpg")#test_img path
+test_img=cv2.imread("E:\\TRY ON VIRTUAL SOFTWARE\\Step 1 Face Recognition\\test\\13.jpg")
 faces_detected,gray_img=fr.faceDetection(test_img)
 print("faces_detected:",faces_detected)
 
 
-#Comment belows lines when running this program second time.Since it saves training.yml file in directory
+#uncomment the lines below when running the module first time as these will train the module
 #faces,faceID=fr.labels_for_training_data('E:\\TRY ON VIRTUAL SOFTWARE\\Step 1 Face Recognition\\train')
 #face_recognizer=fr.train_classifier(faces,faceID)
 #face_recognizer.write('trainingData.yml')
 
 
-#Uncomment below line for subsequent runs
+#comment the lines when running the module first time
 face_recognizer=cv2.face.LBPHFaceRecognizer_create()
-face_recognizer.read('trainingData.yml')#use this to load training data for subsequent runs
+#use this to load training data for subsequent runs
+face_recognizer.read('trainingData.yml')
 
-name={0:"Burak", 1:"Feroze"}#creating dictionary containing names for each label
+#creating dictionary containing names for each label
+name={0:"Burak", 1:"Feroze"}
 
 for face in faces_detected:
     (x,y,w,h)=face
     roi_gray=gray_img[y:y+h,x:x+h]
-    label,confidence=face_recognizer.predict(roi_gray)#predicting the label of given image
+    #predicting the label of given image
+    label,confidence=face_recognizer.predict(roi_gray)
     print("confidence:",confidence)
     print("label:",label)
     fr.draw_rect(test_img,face)
     predicted_name=name[label]
-    if(confidence>37):#If confidence more than 37 then don't print predicted face text on screen
+    #Confidence shouldn't be more than 37, so if it is, module will skip it
+    if(confidence>37):
         continue
+    
+    #calling function to put text on image as label
     fr.put_text(test_img,predicted_name,x,y)
 
+
+#Resize the image after all processing
 resized_img=cv2.resize(test_img,(750,750))
+#Show the resultant image
 cv2.imshow("Face Detection Module",resized_img)
-cv2.waitKey(0)#Waits indefinitely until a key is pressed
+cv2.waitKey(0)
 cv2.destroyAllWindows
